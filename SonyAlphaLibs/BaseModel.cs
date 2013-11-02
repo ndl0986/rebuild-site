ba2e17a8f86067bcbe1faa35c6f07f3b8a30d676
ndl0986 <ndl0986@gmail.com>
2013-11-02 04:53:59 -0700

    update BaseModel.cs

--- SonyAlphaLibs/BaseModel.cs
+++ SonyAlphaLibs/BaseModel.cs
@@ -7,6 +7,30 @@ namespace SonyAlphaLibs
 {
     public abstract class BaseModel
     {
+        private int id;
 
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private DateTime created;
+
+        public DateTime Created
+        {
+            get { return created; }
+            set { created = value; }
+        }
+
+        private DateTime updated;
+
+        public DateTime Updated
+        {
+            get { return updated; }
+            set { updated = value; }
+        }
+        
+        
     }
 }
