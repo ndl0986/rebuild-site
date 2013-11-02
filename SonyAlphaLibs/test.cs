6fca4ae553789246ed06027c254c2a1c23458819
ndl0986 <ndl0986@gmail.com>
2013-11-02 03:19:44 -0700

    add SonyAlphaLibs add test class

---
+++ SonyAlphaLibs/test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SonyAlphaLibs
+{
+    public class test
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        /// <summary>
+        /// Return string that content Id and Name of Object
+        /// </summary>
+        /// <returns></returns>
+        public string getIdName() {
+            return this.id + "_" + this.name;
+        }
+    }
+}
