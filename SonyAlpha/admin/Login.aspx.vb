a82c0a2bed3d0bba323e86c6b761e79d06c81d45
ndl0986 <ndl0986@gmail.com>
2013-11-06 09:51:41 -0800

    update UserService to add method add User and login

--- SonyAlpha/admin/Login.aspx.vb
+++ SonyAlpha/admin/Login.aspx.vb
@@ -1,7 +1,10 @@
 ﻿Imports SonyAlphaLibs
 Public Class Login
     Inherits System.Web.UI.Page
-
+    ' How to add user
+    ' Dim myUser as User
+    ' ...set info for myUser
+    ' call method to add user: myUser.add(myUser, connectionString)
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
--- SonyAlphaLibs/Services/UserServices.cs
+++ SonyAlphaLibs/Services/UserServices.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace SonyAlphaLibs.Services
 {
@@ -9,7 +11,34 @@ namespace SonyAlphaLibs.Services
     {
         public static bool addUser(User user, String connString)
         {
-            
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = "sony_sp_add_user";
+                        cmd.Parameters.AddWithValue("@username", user.UserName);
+                        cmd.Parameters.AddWithValue("@password", user.PassWord);
+                        cmd.Parameters.AddWithValue("@fullname", user.FullName);
+                        cmd.Parameters.AddWithValue("@status", user.Status ? 1 : 0);
+                        cmd.Parameters.AddWithValue("@registered", user.Registered);
+                        cmd.Parameters.AddWithValue("@updated", user.Updated);
+                        SqlParameter returnVal = new SqlParameter("@indentity", SqlDbType.Int);
+                        returnVal.Direction = ParameterDirection.Output;
+                        cmd.Parameters.Add(returnVal);
+
+                        cmd.ExecuteNonQuery();
+                        return ((int)cmd.Parameters["@indentity"].Value == 1);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
             return false;
         }
 
@@ -25,7 +54,41 @@ namespace SonyAlphaLibs.Services
 
         public static bool login(User user, String connString)
         {
-            return true;
+            return login(user.UserName, user.PassWord, connString);
+        }
+
+        public static bool login(String userName, String passWord, String connString)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = "sony_sp_get_user_by_username";
+                        cmd.Parameters.AddWithValue("@username", userName);
+
+                        String getPass = "";
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                getPass = reader.GetString(2);
+                                break;
+                            }
+                        }
+                        return (!String.IsNullOrEmpty(getPass) && getPass == passWord);                        
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //conn.Close();
+                    return false;
+                }
+            }
+            return false;
         }
     }
 }
