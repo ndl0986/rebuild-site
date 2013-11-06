e2720076fee831efd67191df73e872b8fd77b9dc
ndl0986 <ndl0986@gmail.com>
2013-11-06 08:30:14 -0800

    add import SonyAlphaLibs

--- SonyAlpha/admin/Login.aspx.vb
+++ SonyAlpha/admin/Login.aspx.vb
@@ -1,4 +1,5 @@
-﻿Public Class Login
+﻿Imports SonyAlphaLibs
+Public Class Login
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
