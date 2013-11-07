4b3068c5f26f58a66d9c016878ee55a8e164320f
ndl0986 <ndl0986@gmail.com>
2013-11-07 07:16:36 -0800

    update UserServices

--- SonyAlphaLibs/Services/UserServices.cs
+++ SonyAlphaLibs/Services/UserServices.cs
@@ -81,12 +81,45 @@ namespace SonyAlphaLibs.Services
                                 break;
                             }
                         }
-                        rs = (!String.IsNullOrEmpty(getPass) && getPass == passWord);                        
+                        rs = (!String.IsNullOrEmpty(getPass) && getPass == passWord);
                     }
                 }
                 catch (Exception ex)
                 {
                     return rs;
+                }
+            }
+            return rs;
+        }
+
+        internal static bool update(User user, string connString)
+        {
+            bool rs = false;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = "sony_sp_update_user";
+                        cmd.Parameters.AddWithValue("@username", user.UserName);
+                        cmd.Parameters.AddWithValue("@password", user.PassWord);
+                        cmd.Parameters.AddWithValue("@fullname", user.FullName);
+                        cmd.Parameters.AddWithValue("@status", user.Status ? 1 : 0);
+                        cmd.Parameters.AddWithValue("@updated", user.Updated);
+                        SqlParameter returnVal = new SqlParameter("@returnVal", SqlDbType.Int);
+                        returnVal.Direction = ParameterDirection.Output;
+                        cmd.Parameters.Add(returnVal);
+
+                        cmd.ExecuteNonQuery();
+                        rs = ((int)cmd.Parameters["@returnVal"].Value != 0);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return false;
                 }
             }
             return rs;
