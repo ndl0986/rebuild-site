7269ecd7a48ef586ed52fa6378d43410ac8e2092
ndl0986 <ndl0986@gmail.com>
2013-11-07 07:48:38 -0800

    add store get all user group add store update user group

---
+++ SonyAlpha/db/scriptDB_add_store_update_user_group.sql
@@ -0,0 +1,47 @@
+USE [sony_rebuild_alpha]
+GO
+
+/****** Object:  StoredProcedure [dbo].[sony_sp_update_user_group]    Script Date: 11/07/2013 22:46:52 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+-- =============================================
+-- Author:		ndl0986
+-- Create date: 2013-11-07 22:38:00
+-- Description:	
+-- =============================================
+CREATE PROCEDURE [dbo].[sony_sp_update_user_group] 
+	-- Add the parameters for the stored procedure here
+	@id int,
+	@groupname nvarchar(150),
+	@issuper bit,
+	@created datetime,
+	@updated datetime,
+	@returnVal int OUTPUT
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	SET @returnVal = 0;
+    -- Insert statements for procedure here
+	IF EXISTS (SELECT TOP (1) groupname FROM sony_user_group WHERE id=@id)
+	BEGIN
+		SET @updated = GETDATE();
+		UPDATE sony_user_group
+		SET 
+			[groupname] = @groupname,
+			[issuper] = @issuper,
+			[updated] = @updated
+		WHERE [id] = @id
+		SET @returnVal = 1;
+	END
+END
+
+
+GO
+
