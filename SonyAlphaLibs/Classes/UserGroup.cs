5df1c8d19baed805464379f25283ed3688069154
ndl0986 <ndl0986@gmail.com>
2013-11-07 06:47:19 -0800

    add class UserGroup add class UserGroupServices

---
+++ SonyAlphaLibs/Classes/UserGroup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SonyAlphaLibs.Services;
+
+namespace SonyAlphaLibs
+{
+    public class UserGroup : BaseModel, IBaseFunction<UserGroup>
+    {
+        private string groupName;
+
+        public string GroupName
+        {
+            get { return groupName; }
+            set { groupName = value; }
+        }
+
+        private bool isSuper;
+
+        public bool IsSuper
+        {
+            get { return isSuper; }
+            set { isSuper = value; }
+        }
+
+        public bool add(String connString) 
+        {
+            return UserGroupServices.addUserGroup(this, connString);    
+        }
+
+        public bool removeById(int id)
+        {
+            return false;
+        }
+
+        public List<UserGroup> getListAll(String connString)
+        {
+            return new List<UserGroup>();
+        }
+
+        public UserGroup getById(int id, String connString)
+        {
+            return new UserGroup();
+        }
+    }
+}
---
+++ SonyAlphaLibs/Services/UserGroupServices.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace SonyAlphaLibs.Services
+{
+    public static class UserGroupServices
+    {
+        public static bool addUserGroup(UserGroup userGroup, String connString)
+        {
+            bool rs = false;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = "sony_sp_add_user_group";
+                        cmd.Parameters.AddWithValue("@groupname", userGroup.GroupName);
+                        cmd.Parameters.AddWithValue("@issuper", userGroup.IsSuper ? 1 : 0);
+                        cmd.Parameters.AddWithValue("@created", userGroup.Created);
+                        cmd.Parameters.AddWithValue("@updated", userGroup.Updated);
+                        SqlParameter returnVal = new SqlParameter("@indentity", SqlDbType.Int);
+                        returnVal.Direction = ParameterDirection.Output;
+                        cmd.Parameters.Add(returnVal);
+
+                        cmd.ExecuteNonQuery();
+                        rs = ((int)cmd.Parameters["@indentity"].Value != 0);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            return rs;
+        }
+    }
+}
