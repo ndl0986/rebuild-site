03d84fa18d9eaa5d70aad9f1ffb847a95eaebb8d
ndl0986 <ndl0986@gmail.com>
2013-11-09 07:28:27 -0800

    add class Permission, PermissionService

---
+++ SonyAlphaLibs/Classes/Permission.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SonyAlphaLibs.Services;
+
+namespace SonyAlphaLibs
+{
+    public class Permission : BaseModel, IBaseFunction<Permission>
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private bool allowInsert;
+
+        public bool AllowInsert
+        {
+            get { return allowInsert; }
+            set { allowInsert = value; }
+        }
+
+        private bool allowUpdate;
+
+        public bool AllowUpdate
+        {
+            get { return allowUpdate; }
+            set { allowUpdate = value; }
+        }
+
+        private bool allowDelete;
+
+        public bool AllowDelete
+        {
+            get { return allowDelete; }
+            set { allowDelete = value; }
+        }
+
+        public bool add(String connString)
+        {
+            return PermissionServices.addPermission(this, connString);
+        }
+
+        public bool update(String connString)
+        {
+            return PermissionServices.updatePermission(this, connString);
+        }
+
+        public bool delete(String connString)
+        {
+            return PermissionServices.deletePermission(this, connString);
+        }
+
+        public List<Permission> getListAll(String connString)
+        {
+            return PermissionServices.getListAll(connString);
+        }
+
+        public Permission getById(String connString)
+        {
+            return PermissionServices.getById(base.Id, connString);
+        }
+
+        public Permission getByName(String connString)
+        {
+            return PermissionServices.getByName(this.Name, connString);
+        }
+    }
+}
---
+++ SonyAlphaLibs/Services/PermissionServices.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SonyAlphaLibs;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SonyAlphaLibs.Services
+{
+    public static class PermissionServices
+    {
+
+        internal static bool addPermission(Permission permission, string connString)
+        {
+            bool rs = false;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = "sony_sp_add_permission";
+                        cmd.Parameters.AddWithValue("@name", permission.Name);
+                        cmd.Parameters.AddWithValue("@allowinsert", permission.AllowInsert ? 1 : 0);
+                        cmd.Parameters.AddWithValue("@allowupdate", permission.AllowUpdate ? 1 : 0);
+                        cmd.Parameters.AddWithValue("@allowdelete", permission.AllowDelete ? 1 : 0);
+                        cmd.Parameters.AddWithValue("@created", permission.Created);
+                        cmd.Parameters.AddWithValue("@updated", permission.Updated);
+                        SqlParameter returnVal = new SqlParameter("@returnVal", SqlDbType.Int);
+                        returnVal.Direction = ParameterDirection.Output;
+                        cmd.Parameters.Add(returnVal);
+
+                        cmd.ExecuteNonQuery();
+                        rs = ((int)cmd.Parameters["@returnVal"].Value != 0);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            return rs;
+        }
+
+        internal static bool updatePermission(Permission permission, string connString)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static bool deletePermission(Permission permission, string connString)
+        {
+            bool rs = false;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = "sony_sp_delete_permission";
+                        cmd.Parameters.AddWithValue("@name", permission.Name);
+                        SqlParameter returnVal = new SqlParameter("@returnVal", SqlDbType.Int);
+                        returnVal.Direction = ParameterDirection.Output;
+                        cmd.Parameters.Add(returnVal);
+
+                        cmd.ExecuteNonQuery();
+                        rs = ((int)cmd.Parameters["@returnVal"].Value != 0);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            return rs;
+        }
+
+        internal static List<Permission> getListAll(string connString)
+        {
+            List<Permission> lists = new List<Permission>();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = "sony_sp_get_all_permission";
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Permission permission = new Permission();
+                                permission.Id = (int)reader["id"];
+                                permission.Name = reader["name"].ToString();
+                                permission.AllowInsert = reader["allowinsert"].ToString().Equals("1") || reader["allowinsert"].ToString().Equals("True") ? true : false;
+                                permission.AllowUpdate = reader["allowupdate"].ToString().Equals("1") || reader["allowupdate"].ToString().Equals("True") ? true : false;
+                                permission.AllowDelete = reader["allowdelete"].ToString().Equals("1") || reader["allowdelete"].ToString().Equals("True") ? true : false;
+                                permission.Created = (DateTime)reader["created"];
+                                permission.Updated = (DateTime)reader["updated"];
+                                lists.Add(permission);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return new List<Permission>();
+                }
+            }
+            return lists;
+        }
+
+        internal static Permission getById(int id, string connString)
+        {
+            Permission permission = new Permission();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = "sony_sp_get_permission_by_id";
+                        cmd.Parameters.AddWithValue("@id", id);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                permission.Id = (int)reader["id"];
+                                permission.Name = reader["name"].ToString();
+                                permission.AllowInsert = reader["allowinsert"].ToString().Equals("1") || reader["allowinsert"].ToString().Equals("True") ? true : false;
+                                permission.AllowUpdate = reader["allowupdate"].ToString().Equals("1") || reader["allowupdate"].ToString().Equals("True") ? true : false;
+                                permission.AllowDelete = reader["allowdelete"].ToString().Equals("1") || reader["allowdelete"].ToString().Equals("True") ? true : false;
+                                permission.Created = (DateTime)reader["created"];
+                                permission.Updated = (DateTime)reader["updated"];
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return permission;
+                }
+            }
+            return permission;
+        }
+
+        internal static Permission getByName(string name, string connString)
+        {
+            Permission permission = new Permission();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = "sony_sp_get_permission_by_id";
+                        cmd.Parameters.AddWithValue("@name", name);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                permission.Id = (int)reader["id"];
+                                permission.Name = reader["name"].ToString();
+                                permission.AllowInsert = reader["allowinsert"].ToString().Equals("1") || reader["allowinsert"].ToString().Equals("True") ? true : false;
+                                permission.AllowUpdate = reader["allowupdate"].ToString().Equals("1") || reader["allowupdate"].ToString().Equals("True") ? true : false;
+                                permission.AllowDelete = reader["allowdelete"].ToString().Equals("1") || reader["allowdelete"].ToString().Equals("True") ? true : false;
+                                permission.Created = (DateTime)reader["created"];
+                                permission.Updated = (DateTime)reader["updated"];
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return permission;
+                }
+            }
+            return permission;
+        }
+    }
+}
