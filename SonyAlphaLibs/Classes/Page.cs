36569829b6dc0e80ac7ff50d45a78a648e831086
ndl0986 <ndl0986@gmail.com>
2013-11-11 06:21:49 -0800

    add Page.cs, PageServices.cs

---
+++ SonyAlphaLibs/Classes/Page.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SonyAlphaLibs.Services;
+
+namespace SonyAlphaLibs
+{
+    public class Page : BaseModel, IBaseFunction<Page>
+    {
+        #region fields
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        private string longTitle;
+
+        public string LongTitle
+        {
+            get { return longTitle; }
+            set { longTitle = value; }
+        }
+
+        private string pageContent;
+
+        public string PageContent
+        {
+            get { return pageContent; }
+            set { pageContent = value; }
+        }
+
+        private bool isNews;
+
+        public bool IsNews
+        {
+            get { return isNews; }
+            set { isNews = value; }
+        }
+
+        private string seoUrl;
+
+        public string SeoUrl
+        {
+            get { return seoUrl; }
+            set { seoUrl = value; }
+        }
+
+        private bool status;
+
+        public bool Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+        private string creator;
+
+        public string Creator
+        {
+            get { return creator; }
+            set { creator = value; }
+        }
+
+        private string updator;
+
+        public string Updator
+        {
+            get { return updator; }
+            set { updator = value; }
+        } 
+        #endregion
+
+        #region base method
+        public bool add(String connString)
+        {
+            return PageServices.addPage(this, connString);
+        }
+
+        public bool update(String connString)
+        {
+            return PageServices.updatePage(this, connString);
+        }
+
+        public List<Page> getListAll(String connString)
+        {
+            return PageServices.getListAll(connString);
+        }
+
+        public bool removeById(String connString)
+        {
+            return PageServices.removeById(base.Id, connString);
+        }
+
+        public Page getById(String connString)
+        {
+            return PageServices.getById(base.Id, connString);
+        }
+        #endregion
+
+        #region ex method
+        public Page getByName(String connString)
+        {
+            return PageServices.getByName(this.Title, connString);
+        }
+
+        public List<Page> getListPage(int pageNum, int order, String connString)
+        {
+            return PageServices.getListPage(pageNum, order, connString);
+        }
+
+        public List<Page> getListPage(int pageNum, int order, int pageSize, String orderBy, String connString)
+        {
+            return PageServices.getListPage(pageNum, order, pageSize, orderBy, connString);
+        }
+        #endregion
+    }
+}
---
+++ SonyAlphaLibs/Services/PageServices.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SonyAlphaLibs;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace SonyAlphaLibs.Services
+{
+    public class PageServices
+    {
+
+        internal static bool addPage(Page page, string connString)
+        {
+            bool rs = false;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = "sony_sp_add_page";
+                        cmd.Parameters.AddWithValue("@title", page.Title);
+                        cmd.Parameters.AddWithValue("@longtitle", page.LongTitle);
+                        cmd.Parameters.AddWithValue("@pageContent", page.PageContent);
+                        cmd.Parameters.AddWithValue("@isNews", page.IsNews ? 1 : 0);
+                        cmd.Parameters.AddWithValue("@seoUrl", page.SeoUrl);
+                        cmd.Parameters.AddWithValue("@status", page.Status ? 1 : 0);
+                        cmd.Parameters.AddWithValue("@creator", String.IsNullOrEmpty(page.Creator) ? "admin" : page.Creator);
+                        cmd.Parameters.AddWithValue("@updator", String.IsNullOrEmpty(page.Updator) ? "admin" : page.Updator);
+                        SqlParameter returnVal = new SqlParameter("@returnVal", SqlDbType.Int);
+                        returnVal.Direction = ParameterDirection.Output;
+                        cmd.Parameters.Add(returnVal);
+
+                        cmd.ExecuteNonQuery();
+                        rs = ((int)cmd.Parameters["@returnVal"].Value != 0);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            return rs;
+        }
+
+        internal static bool updatePage(Page page, string connString)
+        {
+            bool rs = false;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = "sony_sp_update_page_by_id";
+                        cmd.Parameters.AddWithValue("@id", page.Id);
+                        cmd.Parameters.AddWithValue("@title", page.Title);
+                        cmd.Parameters.AddWithValue("@longtitle", page.LongTitle);
+                        cmd.Parameters.AddWithValue("@pageContent", page.PageContent);
+                        cmd.Parameters.AddWithValue("@isNews", page.IsNews ? 1 : 0);
+                        cmd.Parameters.AddWithValue("@seoUrl", page.SeoUrl);
+                        cmd.Parameters.AddWithValue("@status", page.Status ? 1 : 0);
+                        cmd.Parameters.AddWithValue("@updator", String.IsNullOrEmpty(page.Updator) ? "admin" : page.Updator);
+                        SqlParameter returnVal = new SqlParameter("@returnVal", SqlDbType.Int);
+                        returnVal.Direction = ParameterDirection.Output;
+                        cmd.Parameters.Add(returnVal);
+
+                        cmd.ExecuteNonQuery();
+                        rs = ((int)cmd.Parameters["@returnVal"].Value != 0);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            return rs;
+        }
+
+        internal static List<Page> getListAll(string connString)
+        {
+            #region code
+            List<Page> lists = new List<Page>();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = "sony_sp_get_all_page";
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Page page = new Page();
+                                page.Id = (int)reader["id"];
+                                page.Title = reader["title"].ToString();
+                                page.LongTitle = reader["longtitle"].ToString();
+                                page.PageContent = reader["pageContent"].ToString();
+                                page.IsNews = reader["isNews"].ToString().Equals("1") || reader["isNews"].ToString().Equals("True") ? true : false;
+                                page.SeoUrl = reader["seoUrl"].ToString();
+                                page.Status = reader["status"].ToString().Equals("1") || reader["status"].ToString().Equals("True") ? true : false;
+                                page.Created = (DateTime)reader["created"];
+                                page.Creator = reader["creator"].ToString();
+                                page.Updated = (DateTime)reader["updated"];
+                                page.Updator = reader["updator"].ToString();
+                                lists.Add(page);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return new List<Page>();
+                }
+            }
+            return lists;
+            #endregion
+        }
+
+        internal static bool removeById(int p, string connString)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Page getById(int id, string connString)
+        {
+            #region code
+            Page page = new Page();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = "sony_sp_get_page_by_id";
+                        cmd.Parameters.AddWithValue("@id", id);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                page.Id = (int)reader["id"];
+                                page.Title = reader["title"].ToString();
+                                page.LongTitle = reader["longtitle"].ToString();
+                                page.PageContent = reader["pageContent"].ToString();
+                                page.IsNews = reader["isNews"].ToString().Equals("1") || reader["isNews"].ToString().Equals("True") ? true : false;
+                                page.SeoUrl = reader["seoUrl"].ToString();
+                                page.Status = reader["status"].ToString().Equals("1") || reader["status"].ToString().Equals("True") ? true : false;
+                                page.Created = (DateTime)reader["created"];
+                                page.Creator = reader["creator"].ToString();
+                                page.Updated = (DateTime)reader["updated"];
+                                page.Updator = reader["updator"].ToString();
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return new Page();
+                }
+            }
+            return page;
+            #endregion
+        }
+
+        internal static Page getByName(string name, string connString)
+        {
+            #region code
+            Page page = new Page();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = "sony_sp_get_page_by_title";
+                        cmd.Parameters.AddWithValue("@title", name);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                page.Id = (int)reader["id"];
+                                page.Title = reader["title"].ToString();
+                                page.LongTitle = reader["longtitle"].ToString();
+                                page.PageContent = reader["pageContent"].ToString();
+                                page.IsNews = reader["isNews"].ToString().Equals("1") || reader["isNews"].ToString().Equals("True") ? true : false;
+                                page.SeoUrl = reader["seoUrl"].ToString();
+                                page.Status = reader["status"].ToString().Equals("1") || reader["status"].ToString().Equals("True") ? true : false;
+                                page.Created = (DateTime)reader["created"];
+                                page.Creator = reader["creator"].ToString();
+                                page.Updated = (DateTime)reader["updated"];
+                                page.Updator = reader["updator"].ToString();
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return new Page();
+                }
+            }
+            return page;
+            #endregion
+        }
+
+        internal static List<Page> getListPage(int pageNum, int order, string connString)
+        {
+            #region code
+            List<Page> lists = new List<Page>();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = "sony_sp_get_page_by_num_order";
+                        cmd.Parameters.AddWithValue("@pageNum", pageNum);
+                        cmd.Parameters.AddWithValue("@order", order);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Page page = new Page();
+                                page.Id = (int)reader["id"];
+                                page.Title = reader["title"].ToString();
+                                page.LongTitle = reader["longtitle"].ToString();
+                                page.PageContent = reader["pageContent"].ToString();
+                                page.IsNews = reader["isNews"].ToString().Equals("1") || reader["isNews"].ToString().Equals("True") ? true : false;
+                                page.SeoUrl = reader["seoUrl"].ToString();
+                                page.Status = reader["status"].ToString().Equals("1") || reader["status"].ToString().Equals("True") ? true : false;
+                                page.Created = (DateTime)reader["created"];
+                                page.Creator = reader["creator"].ToString();
+                                page.Updated = (DateTime)reader["updated"];
+                                page.Updator = reader["updator"].ToString();
+                                lists.Add(page);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return new List<Page>();
+                }
+            }
+            return lists;
+            #endregion
+        }
+
+        internal static List<Page> getListPage(int pageNum, int order, int pageSize, string orderBy, string connString)
+        {
+            #region code
+            List<Page> lists = new List<Page>();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = "sony_sp_get_page_by_num_order_size_col";
+                        cmd.Parameters.AddWithValue("@pageNum", pageNum);
+                        cmd.Parameters.AddWithValue("@order", order);
+                        cmd.Parameters.AddWithValue("@size", pageSize);
+                        cmd.Parameters.AddWithValue("@order", order);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Page page = new Page();
+                                page.Id = (int)reader["id"];
+                                page.Title = reader["title"].ToString();
+                                page.LongTitle = reader["longtitle"].ToString();
+                                page.PageContent = reader["pageContent"].ToString();
+                                page.IsNews = reader["isNews"].ToString().Equals("1") || reader["isNews"].ToString().Equals("True") ? true : false;
+                                page.SeoUrl = reader["seoUrl"].ToString();
+                                page.Status = reader["status"].ToString().Equals("1") || reader["status"].ToString().Equals("True") ? true : false;
+                                page.Created = (DateTime)reader["created"];
+                                page.Creator = reader["creator"].ToString();
+                                page.Updated = (DateTime)reader["updated"];
+                                page.Updator = reader["updator"].ToString();
+                                lists.Add(page);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return new List<Page>();
+                }
+            }
+            return lists;
+            #endregion
+        }
+    }
+}
