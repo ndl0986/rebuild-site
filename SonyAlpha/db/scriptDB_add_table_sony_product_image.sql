a90761a3685c339a373b277c2917e7f885ae7188
ndl0986 <ndl0986@gmail.com>
2013-11-16 07:15:13 -0800

    add script create table sony_product_image

---
+++ SonyAlpha/db/scriptDB_add_table_sony_product_image.sql
@@ -0,0 +1,24 @@
+USE [sony_rebuild_alpha]
+GO
+
+/****** Object:  Table [dbo].[sony_product_images]    Script Date: 11/16/2013 22:14:21 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[sony_product_images](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[productId] [int] NOT NULL,
+	[imageUrls] [nvarchar](max) NULL,
+	[created] [datetime] NULL,
+	[updated] [datetime] NULL,
+ CONSTRAINT [PK_sony_product_images] PRIMARY KEY CLUSTERED 
+(
+	[productId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
