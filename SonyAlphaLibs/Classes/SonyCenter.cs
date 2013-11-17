9897da8db2a7927e2a8f5e3c4bc26cd2b153bf87
ndl0986 <ndl0986@gmail.com>
2013-11-17 04:58:49 -0800

    add sonycenter and service

---
+++ SonyAlphaLibs/Classes/SonyCenter.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SonyAlphaLibs.Services;
+
+namespace SonyAlphaLibs
+{
+    public class SonyCenter : BaseModel, IBaseFunction<SonyCenter>
+    {
+        #region fields
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string address;
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        private string phone;
+
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        private string fax;
+
+        public string Fax
+        {
+            get { return fax; }
+            set { fax = value; }
+        }
+
+        private string province;
+
+        public string Province
+        {
+            get { return province; }
+            set { province = value; }
+        }
+
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        private int retailLevel;
+
+        public int RetailLevel
+        {
+            get { return retailLevel; }
+            set { retailLevel = value; }
+        }
+
+        private string centerImage;
+
+        public string CenterImage
+        {
+            get { return centerImage; }
+            set { centerImage = value; }
+        }
+
+        private string openTime;
+
+        public string OpenTime
+        {
+            get { return openTime; }
+            set { openTime = value; }
+        }
+
+        private string closeTime;
+
+        public string CloseTime
+        {
+            get { return closeTime; }
+            set { closeTime = value; }
+        }
+
+        private string longitude;
+
+        public string Longitude
+        {
+            get { return longitude; }
+            set { longitude = value; }
+        }
+
+        private string latitude;
+
+        public string Latitude
+        {
+            get { return latitude; }
+            set { latitude = value; }
+        }
+        
+        #endregion
+
+        #region base method
+        public bool add(String connString)
+        {
+            return SonyCenterServices.addCenter(this, connString);
+        }
+
+        public bool update(String connString)
+        {
+            return SonyCenterServices.updateCenter(this, connString);
+        }
+
+        public bool removeById(String connString)
+        {
+            return SonyCenterServices.removeCenterById(base.Id, connString);
+        }
+
+        public List<SonyCenter> getListAll(String connString)
+        {
+            return SonyCenterServices.getListAllCenter(connString);
+        }
+
+        public SonyCenter getById(String connString)
+        {
+            return SonyCenterServices.getCenterById(base.Id, connString);
+        }
+        #endregion
+    }
+}
---
+++ SonyAlphaLibs/Services/SonyCenterServices.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace SonyAlphaLibs.Services
+{
+    public class SonyCenterServices : BaseService
+    {
+
+        public static bool addCenter(SonyCenter sonyCenter, string connString)
+        {
+            #region code
+            bool rs = false;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = "sony_sp_add_sony_center";
+                        cmd.Parameters.AddWithValue("@name", sonyCenter.Name);
+                        cmd.Parameters.AddWithValue("@address", sonyCenter.Address);
+                        cmd.Parameters.AddWithValue("@phone", sonyCenter.Phone);
+                        cmd.Parameters.AddWithValue("@fax", sonyCenter.Fax);
+                        cmd.Parameters.AddWithValue("@province", sonyCenter.Province);
+                        cmd.Parameters.AddWithValue("@description", sonyCenter.Description);
+                        cmd.Parameters.AddWithValue("@retailLevel", sonyCenter.RetailLevel);
+                        cmd.Parameters.AddWithValue("@centerImage", sonyCenter.CenterImage);
+                        cmd.Parameters.AddWithValue("@opentime", sonyCenter.OpenTime);
+                        cmd.Parameters.AddWithValue("@closetime", sonyCenter.CloseTime);
+                        cmd.Parameters.AddWithValue("@longitude", sonyCenter.Longitude);
+                        cmd.Parameters.AddWithValue("@latitude", sonyCenter.Latitude);
+                        SqlParameter returnVal = new SqlParameter("@returnVal", SqlDbType.Int);
+                        returnVal.Direction = ParameterDirection.Output;
+                        cmd.Parameters.Add(returnVal);
+
+                        cmd.ExecuteNonQuery();
+                        rs = ((int)cmd.Parameters["@returnVal"].Value != 0);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            return rs;
+            #endregion
+        }
+
+        public static bool updateCenter(SonyCenter sonyCenter, string connString)
+        {
+            #region code
+            bool rs = false;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = "sony_sp_update_sony_center";
+                        cmd.Parameters.AddWithValue("@id", sonyCenter.Id);
+                        cmd.Parameters.AddWithValue("@name", sonyCenter.Name);
+                        cmd.Parameters.AddWithValue("@address", sonyCenter.Address);
+                        cmd.Parameters.AddWithValue("@phone", sonyCenter.Phone);
+                        cmd.Parameters.AddWithValue("@fax", sonyCenter.Fax);
+                        cmd.Parameters.AddWithValue("@province", sonyCenter.Province);
+                        cmd.Parameters.AddWithValue("@description", sonyCenter.Description);
+                        cmd.Parameters.AddWithValue("@retailLevel", sonyCenter.RetailLevel);
+                        cmd.Parameters.AddWithValue("@centerImage", sonyCenter.CenterImage);
+                        cmd.Parameters.AddWithValue("@opentime", sonyCenter.OpenTime);
+                        cmd.Parameters.AddWithValue("@closetime", sonyCenter.CloseTime);
+                        cmd.Parameters.AddWithValue("@longitude", sonyCenter.Longitude);
+                        cmd.Parameters.AddWithValue("@latitude", sonyCenter.Latitude);
+                        SqlParameter returnVal = new SqlParameter("@returnVal", SqlDbType.Int);
+                        returnVal.Direction = ParameterDirection.Output;
+                        cmd.Parameters.Add(returnVal);
+
+                        cmd.ExecuteNonQuery();
+                        rs = ((int)cmd.Parameters["@returnVal"].Value != 0);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            return rs;
+            #endregion
+        }
+
+        public static bool removeCenterById(int id, string connString)
+        {
+            #region code
+            bool rs = false;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = "sony_sp_delete_sony_center_by_id";
+                        cmd.Parameters.AddWithValue("@id", id);
+                        SqlParameter returnVal = new SqlParameter("@returnVal", SqlDbType.Int);
+                        returnVal.Direction = ParameterDirection.Output;
+                        cmd.Parameters.Add(returnVal);
+
+                        cmd.ExecuteNonQuery();
+                        rs = ((int)cmd.Parameters["@returnVal"].Value != 0);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            return rs;
+            #endregion
+        }
+
+        public static List<SonyCenter> getListAllCenter(string connString)
+        {
+            #region code
+            List<SonyCenter> lists = new List<SonyCenter>();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = "sony_sp_get_all_sony_center";
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                SonyCenter sonyCenter = new SonyCenter();
+                                sonyCenter.Id = (int)reader["id"];
+                                sonyCenter.Name = reader["name"].ToString(); 
+                                sonyCenter.Address = reader["address"].ToString();
+                                sonyCenter.Phone = reader["phone"].ToString();
+                                sonyCenter.Fax = reader["fax"].ToString();
+                                sonyCenter.Province = reader["province"].ToString();
+                                sonyCenter.Description = reader["description"].ToString();
+                                sonyCenter.RetailLevel = (int)reader["title"];
+                                sonyCenter.CenterImage = reader["CenterImage"].ToString();
+                                sonyCenter.OpenTime = reader["opentime"].ToString();
+                                sonyCenter.CloseTime = reader["closetime"].ToString();
+                                sonyCenter.Longitude = reader["longitude"].ToString();
+                                sonyCenter.Latitude = reader["latitude"].ToString();
+                                sonyCenter.Created = (DateTime)reader["created"];
+                                sonyCenter.Updated = (DateTime)reader["updated"];
+                                lists.Add(sonyCenter);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return new List<SonyCenter>();
+                }
+            }
+            return lists;
+            #endregion
+        }
+
+        public static SonyCenter getCenterById(int id, string connString)
+        {
+            #region code
+            SonyCenter sonyCenter = new SonyCenter();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = "sony_sp_get_sony_center_by_id";
+                        cmd.Parameters.AddWithValue("@id", id);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {                                
+                                sonyCenter.Id = (int)reader["id"];
+                                sonyCenter.Name = reader["name"].ToString();
+                                sonyCenter.Address = reader["address"].ToString();
+                                sonyCenter.Phone = reader["phone"].ToString();
+                                sonyCenter.Fax = reader["fax"].ToString();
+                                sonyCenter.Province = reader["province"].ToString();
+                                sonyCenter.Description = reader["description"].ToString();
+                                sonyCenter.RetailLevel = (int)reader["title"];
+                                sonyCenter.CenterImage = reader["CenterImage"].ToString();
+                                sonyCenter.OpenTime = reader["opentime"].ToString();
+                                sonyCenter.CloseTime = reader["closetime"].ToString();
+                                sonyCenter.Longitude = reader["longitude"].ToString();
+                                sonyCenter.Latitude = reader["latitude"].ToString();
+                                sonyCenter.Created = (DateTime)reader["created"];
+                                sonyCenter.Updated = (DateTime)reader["updated"];
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return new SonyCenter();
+                }
+            }
+            return sonyCenter;
+            #endregion
+        }
+    }
+}
