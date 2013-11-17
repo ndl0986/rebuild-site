a09faf463fec9cf4b2359e46167edb261677a821
ndl0986 <ndl0986@gmail.com>
2013-11-17 01:56:46 -0800

    update script

--- SonyAlpha/db/scriptDB_drop_and_create_table_sony_center.sql
+++ SonyAlpha/db/scriptDB_drop_and_create_table_sony_center.sql
@@ -11,7 +11,7 @@ GO
 USE [sony_rebuild_alpha]
 GO
 
-/****** Object:  Table [dbo].[sony_center]    Script Date: 11/17/2013 16:53:47 ******/
+/****** Object:  Table [dbo].[sony_center]    Script Date: 11/17/2013 16:56:24 ******/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[sony_center]') AND type in (N'U'))
 DROP TABLE [dbo].[sony_center]
 GO
@@ -19,11 +19,14 @@ GO
 USE [sony_rebuild_alpha]
 GO
 
-/****** Object:  Table [dbo].[sony_center]    Script Date: 11/17/2013 16:53:47 ******/
+/****** Object:  Table [dbo].[sony_center]    Script Date: 11/17/2013 16:56:24 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
 GO
 
 CREATE TABLE [dbo].[sony_center](
@@ -35,7 +38,7 @@ CREATE TABLE [dbo].[sony_center](
 	[province] [nvarchar](50) NULL,
 	[description] [text] NULL,
 	[retailLevel] [int] NOT NULL,
-	[avatar] [int] NULL,
+	[centerImage] [varchar](500) NULL,
 	[opentime] [nvarchar](50) NULL,
 	[closetime] [nvarchar](50) NULL,
 	[longitude] [nvarchar](20) NULL,
@@ -48,6 +51,9 @@ CREATE TABLE [dbo].[sony_center](
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
+GO
+
+SET ANSI_PADDING OFF
 GO
 
 ALTER TABLE [dbo].[sony_center] ADD  CONSTRAINT [DF_sony_center_retailLevel]  DEFAULT ((0)) FOR [retailLevel]
