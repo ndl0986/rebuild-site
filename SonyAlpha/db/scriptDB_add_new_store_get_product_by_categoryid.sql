101b135e97316152442f40ef855832d8af322c00
ndl0986 <ndl0986@gmail.com>
2013-11-23 08:16:05 -0800

    add new script

---
+++ SonyAlpha/db/scriptDB_add_new_store_get_product_by_categoryid.sql
@@ -0,0 +1,31 @@
+USE [sony_rebuild_alpha]
+GO
+
+/****** Object:  StoredProcedure [dbo].[sony_sp_get_all_product]    Script Date: 11/23/2013 23:06:26 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		ndl0986
+-- Create date: 
+-- Description:	
+-- =============================================
+CREATE PROCEDURE [dbo].[sony_sp_get_all_product_by_category] 
+	-- Add the parameters for the stored procedure here
+	@categoryId int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT * FROM sony_product WHERE [categoryId]=@categoryId;
+END
+
+GO
+
+
